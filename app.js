7785cf33b21aabbe6be37c296e66dc03b00dec01
YIPG <around.mr.child@gmail.com>
2019-05-31 17:47:58 +0900

    cookie

--- app.js
+++ app.js
@@ -1,53 +1,14 @@
+const cookieParser = require("cookie-parser")
 const express = require("express")
-const jwt = require("jsonwebtoken")
-
 const app = express()
+app.use(cookieParser())
 
-app.get("/api", (req, res) => {
-  res.json({
-    massage: "Welcome to the API"
-  })
+app.get("/", (req, res) => {
+  res.cookie("name", "express").send("cookie set")
 })
 
-app.post("/api/posts", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err, authData) => {
-    if (err) {
-      res.sendStatus(403)
-    } else {
-      res.json({
-        message: "Post created...",
-        authData
-      })
-    }
-  })
+app.get("/unko", (req, res) => {
+  res.send(req.cookies)
 })
 
-app.post("/api/login", (req, res) => {
-  const user = {
-    id: 1,
-    username: "brad",
-    email: "example@example.com"
-  }
-
-  jwt.sign({ user }, "secretkey", (err, token) => {
-    res.json({
-      token
-    })
-  })
-})
-
-// Verify Token
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"]
-  if (typeof bearerHeader !== "undefined") {
-    const bearer = bearerHeader.split(" ")
-    const bearerToken = bearer[1]
-    req.token = bearerToken
-    next()
-  } else {
-    console.log("カスが")
-    res.sendStatus(403)
-  }
-}
-
-app.listen(5000, () => console.log("サーバー起動 at 5000"))
+app.listen(5000, () => console.log("listening at localhost:5000"))
